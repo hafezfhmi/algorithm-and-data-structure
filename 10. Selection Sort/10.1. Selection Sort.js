fbd310bf5e7eff30a461e20fc46021d48b792696
hafezfhmi <hafezfahmi2000@gmail.com>
2022-08-18 23:07:22 +0800

    10. Selection Sort

---
+++ 10. Selection Sort/10.1. Selection Sort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let lowest = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([2, 66, 22, 88, -22, 11, 9]));
